1edb7fc9057fbe53f7e4c1b84a5518afcc4670ef
gyim1230 <gyim1230@users.noreply.github.com>
2021-07-26 10:31:09 +0000

    Automated deployment: Mon Jul 26 10:31:09 UTC 2021 a37d051471c0fa4037643326871b138993a544b2

--- server/src/repositories/questionRepository.ts
+++
@@ -1,9 +0,0 @@
-import QuestionModel from '../models/questionModel';
-
-import Repository from './repository';
-
-export default class QuestionRepository extends Repository {
-  constructor() {
-    super({ Model: new QuestionModel() });
-  }
-}
--- server/start.sh
+++
@@ -1,3 +0,0 @@
-rm -rf dist
-tsc
-node dist/app.js
