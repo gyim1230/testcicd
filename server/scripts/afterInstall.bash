8639f3fe69d1e6ba7fb67c94af644fe947277d35
gyim1230 <gyim1230@gmail.com>
2021-08-07 22:31:32 +0900

    deploy

---
+++ server/scripts/afterInstall.bash
@@ -0,0 +1,1 @@
+pm2 start dev-answer/server/src/app.ts
---
+++ server/scripts/beforeInstall.bash
@@ -0,0 +1,7 @@
+pm2 stop app
+
+if [ -d /home/ubuntu/dev-answer ]; then
+    rm -rf /home/ubuntu/dev-answer
+fi
+
+mkdir -vp /home/ubuntu/dev-answer
