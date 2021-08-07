b037997d507f44427984d847c2236fc1ed6f4579
gyim1230 <gyim1230@gmail.com>
2021-08-08 00:16:28 +0900

    deploy

--- server/scripts/afterInstall.bash
+++ server/scripts/afterInstall.bash
@@ -1,9 +1,6 @@
 #!/bin/bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-mkdir -vp this
-
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # Thsis loads nvm bash_completion
 
 pm2 start /home/ubuntu/dev-answer/server/src/app.ts
